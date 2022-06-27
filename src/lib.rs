20b227dbad1438e6f580df97611d03354341a9fa
Lictor Guzman <lctrgzmn@gmail.com>
2022-06-27 02:28:01 -0500

    graphemes moved to their own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-mod ipa;
+pub mod graphemes;
+pub mod ipa;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-        println!("{:?}", ipa::LAT_FRICATIVES)
+    fn lateral_fricatives() {
+        let voiceless_lateral_fricative = r"ɬ";
+        let voiced_lateral_fricative = r"ɮ";
+        assert!(voiceless_lateral_fricative == graphemes::LAT_FRICATIVES[0]);
+        assert!(voiced_lateral_fricative == graphemes::LAT_FRICATIVES[1]);
     }
 }
